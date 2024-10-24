1824c280c107d8a2d876011b8357bfd972a85ab4
pretzil-regine-alic <famie.project@gmail.com>
2024-10-24 11:10:33 +0800

    config update

--- lib/services/config.dart
+++ lib/services/config.dart
@@ -1,7 +1,7 @@
 //filename:../services/config.dart
 class Config {
-  static const String baseUrl = 'http://192.168.1.6:4452';
-  static const String websocketUrl = 'ws://192.168.1.6:4453';
+  static const String baseUrl = 'https://famkid-backend-app-e13f7fa358c9.herokuapp.com';
+  static const String websocketUrl = 'wss://famkid-backend-app-e13f7fa358c9.herokuapp.com';
   //static const String baseUrl = 'http://192.168.100.235:4452';
   //static const String websocketUrl = 'ws://192.168.100.235:4453';
 }
